9471535ddc9f1c51ed6d0d5a438683c1f9bbfbf0
BrooksPoltl <bpoltl1@gmail.com>
2019-06-10 19:27:28 -0500

    added Snack class

---
+++ src/snackBar/Snack.java
@@ -0,0 +1,77 @@
+package snackBar;
+
+public class Snack
+{
+    private int id;
+    private static int maxId = 0;
+    private String name;
+    public double cost;
+    private int quantity;
+    private int vendingMachineID;
+
+    public Snack(String name, int quantity,double cost, int vendingMachineID)
+    {
+        maxId++;
+        this.id = maxId;
+        this.cost = cost;
+        this.name = name;
+        this.quantity = quantity;
+        this.vendingMachineID = vendingMachineID;
+
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public double getCost()
+    {
+        return cost;
+    }
+    public void setCost(double cost)
+    {
+        this.cost = cost;
+    }
+
+    public int getVendingMachineID()
+    {
+        return vendingMachineID;
+    }
+    public void setVendingMachineID(int id)
+    {
+        this.vendingMachineID = id;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+    public void addQuantity(int amt)
+    {
+        private int total = amt + getQuantity()
+        this.quantity = total;
+    }
+
+    public void buySnack(int amt)
+    {
+        private int total = getQuantity()-amt;
+        this.quantity = total;
+    }
+    
+    public double getTotalCost(int quant)
+    {
+        private double total = quant * getCost()
+        return total;
+    }
+
+}
